449dff1705dc4c490908c2601253ea43e50c49fc
legend80s <liuchzong@qq.com>
2021-07-06 22:33:25 +0800

    feat: add clear and set-root sub cmd

---
+++ cmd/set-search-dir.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/legend80s/go-change-dir/utils"
+)
+
+func SetSearchDir(dbFilepath string, searchDir string, verbose bool) (bool, error) {
+	if verbose {
+		fmt.Println("new search dir", searchDir)
+	}
+
+	db := utils.ReadDB(dbFilepath, verbose)
+
+	return utils.SaveWorkspaceToDB(dbFilepath, db, searchDir, verbose)
+}
+
+// How to Delete or Remove a File in Golang?
+func ClearDB(dbFilepath string, verbose bool) {
+	err := os.Remove(dbFilepath)
+
+	if err != nil {
+		fmt.Println("Remove cache", dbFilepath, "failed:", err)
+	}
+}
+
+func GetSearchDir() string {
+	data := utils.ReadDB(utils.DBFilepath, false)
+
+	if data.Workspace != "" {
+		return data.Workspace
+	}
+
+	return utils.Normalize("~/workspace")
+}
