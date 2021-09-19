6acd03bbdaf07337f28d634fdc76413cc899e098
legend80s <liuchzong@qq.com>
2021-09-19 16:03:20 +0800

    test: take directory nested level into account

--- utils/fs_test.go
+++ utils/fs_test.go
@@ -6,6 +6,41 @@ import (
 	"github.com/legend80s/go-change-dir/utils"
 )
 
+func TestGetDirectoryLevel(t *testing.T) {
+	t.Run("testname1", func(t *testing.T) {
+		input := "~/workspace/legend80s/gallery/dist/mac/UGallery.app/Contents/Resources/mr.lproj"
+		expected := 10
+
+		actual := utils.GetDiretoryLevel(input)
+
+		if actual != expected {
+			t.Errorf("Level should be %d but got %d", expected, actual)
+		}
+	})
+
+	t.Run("testname2", func(t *testing.T) {
+		input := "/Users/liuchuanzong/workspace/legend80s/cdi"
+		expected := 5
+
+		actual := utils.GetDiretoryLevel(input)
+
+		if actual != expected {
+			t.Errorf("Level should be %d but got %d", expected, actual)
+		}
+	})
+
+	t.Run("testname3", func(t *testing.T) {
+		input := "dir-lab/amuser-low-info"
+		expected := 2
+
+		actual := utils.GetDiretoryLevel(input)
+
+		if actual != expected {
+			t.Errorf("Level should be %d but got %d", expected, actual)
+		}
+	})
+}
+
 func TestMatch(t *testing.T) {
 	var tests = []struct {
 		paths          []string
@@ -13,6 +48,140 @@ func TestMatch(t *testing.T) {
 		keyword        string
 		want           string
 	}{
+		// level should be taken into account. always choose the least level until level diff > 2
+		{
+			paths: []string{
+				"/Users/name/workspace/legend80s/gallery/dist/mac/UGallery.app/Contents/11/mr.lproj",
+				// level 6
+				"/Users/name/workspace/6/paytm/MiniRecharge",
+			},
+			keyword: "mr",
+			// we choose "MiniRecharge" though "mr.lproj"'s priority is higher than MiniRecharge abbr
+			// but "mr.lproj" is too long. diff 11 - 6 = 5 > 2
+			want:           "/Users/name/workspace/6/paytm/MiniRecharge",
+			matchedPathLen: 2,
+		},
+		// choose "MiniRecharge" because diff 6 - 2 = 4 > 2
+		{
+			paths: []string{
+				"/Users/name/workspace/6/paytm/mr",
+				"/Users/MiniRecharge",
+			},
+			keyword:        "mr",
+			want:           "/Users/MiniRecharge",
+			matchedPathLen: 2,
+		},
+		// choose "MiniRecharge" because diff 6 - 3 = 3 > 2
+		{
+			paths: []string{
+				"/Users/name/workspace/6/paytm/mr",
+				"/Users/3/MiniRecharge",
+			},
+			keyword:        "mr",
+			want:           "/Users/3/MiniRecharge",
+			matchedPathLen: 2,
+		},
+		{
+			paths: []string{
+				"/Users/name/workspace/6/paytm/mr",
+				"/Users/3/MiniRecharge",
+				"/Users/MiniRecharge",
+			},
+			keyword:        "mr",
+			want:           "/Users/MiniRecharge",
+			matchedPathLen: 3,
+		},
+		// choose "mr" because diff 6 - 4 = 2 not > 2
+		{
+			paths: []string{
+				"/Users/name/workspace/6/paytm/mr",
+				"/Users/3/4/MiniRecharge",
+			},
+			keyword:        "mr",
+			want:           "/Users/name/workspace/6/paytm/mr",
+			matchedPathLen: 2,
+		},
+		// choose "mr" because diff 6 - 5 = 1 not > 2
+		{
+			paths: []string{
+				"/Users/name/workspace/6/paytm/mr",
+				"/Users/3/4/5/MiniRecharge",
+			},
+			keyword:        "mr",
+			want:           "/Users/name/workspace/6/paytm/mr",
+			matchedPathLen: 2,
+		},
+		// choose "mr" because diff 6 - 6 = 0 not > 2
+		{
+			paths: []string{
+				"/Users/name/workspace/6/paytm/mr",
+				"/Users/3/4/5/6/MiniRecharge",
+			},
+			keyword:        "mr",
+			want:           "/Users/name/workspace/6/paytm/mr",
+			matchedPathLen: 2,
+		},
+		// choose "mr" because diff 6 - 7 = -1 not > 2
+		{
+			paths: []string{
+				"/Users/name/workspace/6/paytm/mr",
+				"/Users/3/4/5/6/7/MiniRecharge",
+			},
+			keyword:        "mr",
+			want:           "/Users/name/workspace/6/paytm/mr",
+			matchedPathLen: 2,
+		},
+
+		{
+			paths: []string{
+				// level 11
+				"/Users/name/workspace/legend80s/gallery/dist/mac/UGallery.app/Contents/11/mr.lproj",
+				// level 8
+				"/Users/name/workspace/alipay/paytm/7/8/MiniRecharge",
+			},
+			keyword: "mr",
+			// we choose level 8 because 11 - 8 > 2
+			want:           "/Users/name/workspace/alipay/paytm/7/8/MiniRecharge",
+			matchedPathLen: 2,
+		},
+		{
+			paths: []string{
+				// level 11
+				"/Users/name/workspace/legend80s/gallery/dist/mac/UGallery.app/Contents/11/mr.lproj",
+				// level 9
+				"/Users/name/workspace/alipay/paytm/7/8/9/MiniRecharge",
+			},
+			keyword: "mr",
+			// we choose level 11 because 11 - 9 not > 2
+			want:           "/Users/name/workspace/legend80s/gallery/dist/mac/UGallery.app/Contents/11/mr.lproj",
+			matchedPathLen: 2,
+		},
+
+		{
+			paths: []string{
+				// level 11
+				"/Users/name/workspace/legend80s/gallery/dist/mac/UGallery.app/Contents/11/mr.lproj",
+				// level 10
+				"/Users/name/workspace/alipay/paytm/7/8/9/10/MiniRecharge",
+			},
+			keyword: "mr",
+			// we choose level 11 because 11 - 9 not > 2
+			want:           "/Users/name/workspace/legend80s/gallery/dist/mac/UGallery.app/Contents/11/mr.lproj",
+			matchedPathLen: 2,
+		},
+
+		{
+			paths: []string{
+				"/Users/liuchuanzong/workspace/alipay/paytm/MiniRecharge",
+				"/Users/liuchuanzong/workspace/alipay/pc/openmonitor-bulls/src/page/MonitorRule",
+				"/Users/liuchuanzong/workspace/legend80s/gallery/dist/mac/UGallery.app/Contents/Frameworks/Electron Framework.framework/Versions/A/Resources/mr.lproj",
+				"/Users/liuchuanzong/workspace/legend80s/gallery/dist/mac/UGallery.app/Contents/Resources/mr.lproj",
+			},
+			keyword:        "mr",
+			want:           "/Users/liuchuanzong/workspace/alipay/paytm/MiniRecharge",
+			matchedPathLen: 4,
+		},
+
 		// full match against base name
 		{paths: []string{"xxx/balance", "xxx/mini-balance"}, keyword: "balance", want: "xxx/balance", matchedPathLen: 2},
 
@@ -26,8 +195,8 @@ func TestMatch(t *testing.T) {
 				"dir-lab/long/hello-alitest",
 				"dir-lab/long-long-long-long-long/ali",
 			},
-			keyword: "ali",
-			want: "dir-lab/long/ali",
+			keyword:        "ali",
+			want:           "dir-lab/long/ali",
 			matchedPathLen: 6,
 		},
 
@@ -39,9 +208,20 @@ func TestMatch(t *testing.T) {
 				"xxx/ali-abtest-long-long-dir",
 				"xxx/balance",
 			},
-			keyword: "ali",
-			want: "xxx/ali-abtest-long-long-dir",
+			keyword:        "ali",
+			want:           "xxx/ali-abtest-long-long-dir",
 			matchedPathLen: 3,
+		},
+
+		// inverse input
+		{
+			paths: []string{
+				"xxx/ali-abtest-long-long-dir",
+				"x/amuser-low-info",
+			},
+			keyword:        "ali",
+			want:           "xxx/ali-abtest-long-long-dir",
+			matchedPathLen: 2,
 		},
 
 		// full match against path suffix
