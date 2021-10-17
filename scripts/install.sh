585820344a2fd6b2ca67055a1161f8d3b904b924
legend80s <liuchzong@qq.com>
2021-10-17 13:57:47 +0800

    refactor: change variable to constant, local; define color functions; refactor usage function to reduce nested level; double quote arguments for safety

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,23 +4,48 @@
 # https://stackoverflow.com/questions/2870992/automatic-exit-from-bash-shell-script-on-error
 set -e
 
-RED='\033[31m'
-GREEN='\033[32m'
-YELLOW='\033[33m'
-CYAN='\033[96m'
-RED='\033[0;31m'
-RESET='\033[0m'
+# The declare command is used to create the constant variable.
+# https://bash.cyberciti.biz/guide/Local_variable
+# In MacOSX, using \x1B instead of \e. \033 would be ok for all platforms.
+# https://stackoverflow.com/a/28938235
+declare -r RED='\033[31m'
+declare -r GREEN='\033[32m'
+declare -r YELLOW='\033[33m'
+declare -r CYAN='\033[96m'
+declare -r RESET='\033[0m'
+
+# https://bash.cyberciti.biz/guide/Pass_arguments_into_a_function
+# @example
+# hello=$(cyan cdi)
+# echo hello
+# echo "🚀 Try $(cyan \`cdi\`) in your terminal."
+function cyan() {
+  # cann not use `return`
+  # becuase `return` command in Bash can only return status codes which are obviously integer values
+  # # https://mezzantrop.wordpress.com/2018/06/27/a-nice-way-to-return-string-values-from-functions-in-bash/
+  echo "${CYAN}$@${RESET}"
+}
+
+function red() {
+  echo "${RED}$@${RESET}"
+}
+
+function green() {
+  echo "${GREEN}$@${RESET}"
+}
 
 success() {
-  cdipath=$1
-  toUpdate=false; [ "$2" = "update" ] && toUpdate=true
+  # All function variables are local. This is a good programming practice.
+  # https://bash.cyberciti.biz/guide/Local_variable
+  local cdipath="$1"
+  local toUpdate=false; [ "$2" = "update" ] && toUpdate=true
 
   # make it executable
   chmod +x $cdipath && xattr -c $cdipath &&
-    [ $toUpdate = false ] && echo "\n--- cdi install begin ----\n\n${GREEN}✅ $cdipath${RESET} has been made executable.${RESET}"
+    [ $toUpdate = false ] && echo "\n                 --- cdi install begin ----\n\n$(green "✅ $cdipath") has been made executable."
 
   if [ $toUpdate = true ]; then
-    echo "${GREEN}\`cdi\` Updated.${RESET}"
+    green '`cdi` Updated.'
     return
   fi
 
@@ -61,22 +86,27 @@ alias cdi-stat=\"\$cdipath stat\"
 alias cdi-stat-clear=\"\$cdipath stat --clear && echo -n 'Clear cache success. ' && cdi-stat\"
 ## --- cdi end ---
 " >> ~/.zshrc &&
-  echo "✅ Shell functions ${GREEN}\`cdi\`${RESET} and ${GREEN}\`codi\`${RESET} has been added to your ${GREEN}~/.zshrc${RESET}"
-  echo "${GREEN}🎉 You are ready to go to use \`cdi\` and \`codi\`.${RESET}"
-  echo "🚀 Try ${GREEN}\`$ cdi cdi\`${RESET} in your terminal."
-  echo "\n--- cdi install end ----\n"
+  echo "✅ Shell functions $(cyan \`cdi\`) and many more functions have been added to your $(green ~/.zshrc)"
+  echo "🎉 You are ready to go to use $(cyan \`cdi\`) / $(cyan \`codi\`) / $(cyan \`cdi-echo\`) / $(cyan \`cdi-stat\`) / $(cyan \`cdi-stat-clear\`)."
+  echo "🚀 Try $(cyan '$ cdi any_directory_you_like') in your terminal."
+  echo "\n                 --- cdi install end ----\n"
 }
 
-# https://stackoverflow.com/questions/6482377/check-existence-of-input-argument-in-a-bash-shell-script
-if [ -z "$1" ]; then
-  echo "${RED}🚨 Empty params.${RESET}\n"
+usage() {
+  red '🚨 Empty params. Usage:\n'
+  green 'sh scripts/install.sh ~/path/to/downloaded/cdi'
 
-  echo "${GREEN}Example: \n"
-  echo "sh scripts/install.sh ~/path/to/downloaded/cdi${RESET}"
+  exit 2
+}
+
+# https://bash.cyberciti.biz/guide/Local_variable
+[ $# -eq 0 ] && usage
+
+# https://stackoverflow.com/questions/6482377/check-existence-of-input-argument-in-a-bash-shell-script
+
+if [ -f "$1" ]; then
+  # https://stackoverflow.com/questions/10067266/when-should-i-wrap-quotes-around-a-shell-variable
+  success "$1" "$2"
 else
-  if [ -f "$1" ]; then
-    success $1 $2
-  else
-    echo "${RED}❌ $1 not exists!${RESET}"
-  fi
+  red "❌ $1 not exists!"
 fi
